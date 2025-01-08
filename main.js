11b60e342bc4834c798114fb39e096b896d565f3
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2025-01-09 03:47:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__content h4", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".intro__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(
+  ".about__row:nth-child(3) .about__image img, .about__row:nth-child(5) .about__image img",
+  {
+    ...scrollRevealOption,
+    origin: "left",
+  }
+);
+ScrollReveal().reveal(".about__row:nth-child(4) .about__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".about__content span", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__content h4", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".about__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".product__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  },
+  breakpoints: {
+    // when window width is <= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is <= 540px
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    // when window width is < 540px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    }
+  }
+});
+
+ScrollReveal().reveal(".instagram__grid img", {
+  duration: 1000,
+  interval: 500,
+});
